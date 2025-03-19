9a5c9ddc110b9425a1deeb3637393d9425de0f0f
GaviBueno <gabri.buenoreyes@alum.uca.es>
2025-03-19 20:07:58 +0100

    Entrega1

---
+++ src/code.c
@@ -0,0 +1,16 @@
+/*Metodo IA(proporciones, barcos){
+    Proporciones => Crear matriz formada por 0 (o recibirla porq ya se ha creado)
+    Crear una posicion random
+    Enviar la "posicion random" al mapa y que me diga que hay alli
+    Si recibo "tocado" ponemos un 2
+        Ataca alrededor y para buscar al barco
+        Repite el bucle hasta recibir hundido
+        Elimina el barco de la lista "barcos"
+    Si es agua repete el bucle 
+}
+*/
+
+void Jugador0(int n){
+// n es la proporcion del mapa con el que vamos a jugar
+// con que tipo de dato vamos a enviar los barcos?
+}
